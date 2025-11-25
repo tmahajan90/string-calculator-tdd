5a82bed732fc0e082fcf45f0433c20d9a60be300
Tarun Mahajan <mahajan.tarun1990@gmail.com>
2025-11-25 15:31:00 +0530

    add validation for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -11,6 +11,10 @@ class StringCalculator
 
     numbers = numbers.gsub("\n", delimiter)
     nums = numbers.split(delimiter).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+
     nums.sum
   end
 end

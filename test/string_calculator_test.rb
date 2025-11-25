4b31d135d0cfca2ca69db06fdfada4c88b74dd35
Tarun Mahajan <mahajan.tarun1990@gmail.com>
2025-11-25 15:44:00 +0530

    add more test cases

--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -20,23 +20,29 @@ describe StringCalculator do
 
   it "handles multiple numbers" do
     _(@calculator.add("1,2,3,4")).must_equal 10
+    _(@calculator.add("1,2,3,4,5")).must_equal 15
   end
 
   it "handles newlines as delimiters" do
     _(@calculator.add("1\n2,3")).must_equal 6
+    _(@calculator.add("1\n2\n3,4")).must_equal 10
   end
 
   it "supports custom delimiters" do
     _(@calculator.add("//;\n1;2")).must_equal 3
+    _(@calculator.add("//|\n1|2|3")).must_equal 6
+    _(@calculator.add("//@\n2@3@5")).must_equal 10
   end
 
   it "throws exception for negative numbers" do
     error = _ { @calculator.add("-1") }.must_raise RuntimeError
     _(error.message).must_match(/negative numbers not allowed/)
+    _(error.message).must_match(/-1/)
   end
 
   it "shows all negatives in exception message" do
     error = _ { @calculator.add("-1,2,-3") }.must_raise RuntimeError
+    _(error.message).must_match(/negative numbers not allowed/)
     _(error.message).must_match(/-1/)
     _(error.message).must_match(/-3/)
   end

6c292599ae8a86c43bf3f2d088613f2017580954
Tarun Mahajan <mahajan.tarun1990@gmail.com>
2025-11-25 14:14:00 +0530

    add test for newlines

--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -17,4 +17,12 @@ describe StringCalculator do
   it "returns sum for two numbers" do
     _(@calculator.add("1,5")).must_equal 6
   end
+
+  it "handles multiple numbers" do
+    _(@calculator.add("1,2,3,4")).must_equal 10
+  end
+
+  it "handles newlines as delimiters" do
+    _(@calculator.add("1\n2,3")).must_equal 6
+  end
 end

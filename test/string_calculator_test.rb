d7f4139185f8a8484c0481c1dc59facfc6318463
Tarun Mahajan <mahajan.tarun1990@gmail.com>
2025-11-25 13:43:00 +0530

    add tests for single and two numbers

--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -9,4 +9,12 @@ describe StringCalculator do
   it "returns zero for empty string" do
     _(@calculator.add("")).must_equal 0
   end
+
+  it "returns the number for single input" do
+    _(@calculator.add("1")).must_equal 1
+  end
+
+  it "returns sum for two numbers" do
+    _(@calculator.add("1,5")).must_equal 6
+  end
 end

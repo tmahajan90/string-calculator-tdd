7dad20dc5749bc77d4e8282ae364c14604eadcc5
Tarun Mahajan <mahajan.tarun1990@gmail.com>
2025-11-25 14:22:00 +0530

    handle newlines as delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     
+    numbers = numbers.gsub("\n", ",")
     nums = numbers.split(",").map(&:to_i)
     nums.sum
   end
